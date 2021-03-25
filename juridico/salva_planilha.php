f520281a5b15974e2fa5abe3cfa6bfb28be4f13c
Renan Brito <renan.brito@qualidoc.com.br>
2021-03-25 15:00:59 -0300

    Finalização da implementação back-end pra subir a planilha de consulta de logs

--- juridico/salva_planilha.php
+++ juridico/salva_planilha.php
@@ -32,24 +32,25 @@ else {
        $success = false;
     }
     else {
-       $msg = 'upload/'.$_FILES['file']['name'];
-       if(move_uploaded_file($_FILES['file']['tmp_name'], '../upload/' . $_FILES['file']['name'])) {
-           $file = fopen("../upload/{$_FILES['file']['name']}", "r");
+       $filenamecsv = '../upload/' . $_FILES['file']['name'];
+       if(move_uploaded_file($_FILES['file']['tmp_name'], $filenamecsv)) {
+           $file = fopen($filenamecsv, "r");
            $sql = "INSERT INTO produtos_juridico (codigo, descricao, data, url_monitor, preco_custo, website_monitorado, url_monitorado, hora, preco_oferta) VALUES ";
            $i = 0;
            while (($getData = fgetcsv($file, 10000, "|")) !== false) {
              if($i > 0) {
                $fields = explode('";"', $getData[0]);
-             if(strpos(trim(explode(';"', $fields[0])[0]), 'IA-') === false) {
+               if(strpos(trim(explode(';"', $fields[0])[0]), 'IA-') === false) {
                  $fields_array = array();
+                 $data_csv = trim(implode('-', array_reverse(explode('/', str_replace('"', '', $fields[25])))));
                  array_push($fields_array,
                                            trim(explode(';"', $fields[0])[0]) ?? 0,
-                                           '"'.trim(utf8_encode(explode(';"', $fields[0])[1])).'"' ?? '',
+                                           '"'.trim(explode(';"', $fields[0])[1]).'"' ?? '',
                                            '"'.trim(implode('-', array_reverse(explode('/', str_replace('"', '', $fields[25]))))).'"' ?? '""',
-                                           '"'.trim(utf8_encode(str_replace('";', '', $fields[30]))).'"' ?? '',
+                                           '"'.trim(str_replace('";', '', $fields[30])).'"' ?? '',
                                            str_replace('"', '', $fields[16]) != "" ? trim(str_replace(",", ".", str_replace('"', '', $fields[16]))) : 0,
-                                           '"'.trim(utf8_encode($fields[23])).'"' ?? '',
-                                           '"'.trim(utf8_encode($fields[24])).'"' ?? '',
+                                           '"'.trim($fields[23]).'"' ?? '',
+                                           '"'.trim($fields[24]).'"' ?? '',
                                            '"'.trim($fields[26]).'"' ?? '',
                                            str_replace('"', '', $fields[28]) != "" ? trim(str_replace(",", ".", str_replace('"', '', $fields[28]))) : 0);
                  $sql .= "(".implode(", ", $fields_array)."),\n";
@@ -60,17 +61,25 @@ else {
            fclose($file);
            $sql = substr($sql, 0, -2);
            $sql .= ";";
-           $file_dump = fopen('../export_sql/data.txt', 'w');
+           $dump = "../export_sql/data_".$data_csv.".txt";
+           $file_dump = fopen($dump, 'w');
            fwrite($file_dump, $sql);
            fclose($file_dump);
-           $msg = 'Arquivo dump gerado!';
-           $success = true;
+           if(trim(shell_exec("mysql -u$DATABASE_USER -p'$DATABASE_PASS' $DATABASE_NAME < $dump 2>&1"))
+                        == "mysql: [Warning] Using a password on the command line interface can be insecure.") {
+             $msg = 'Planilha atualizada com sucesso!';
+             $success = true;
+           }
+           else {
+             $msg = 'Não foi possível salvar o dump!';
+             $success = false;
+           }
        }
        else {
            $msg = 'Não foi possível salvar o arquivo!';
            $success = false;
        }
+       unlink($filenamecsv);
     }
-
     echo json_encode(array('success' => $success, 'msg' => $msg));
 }?>
