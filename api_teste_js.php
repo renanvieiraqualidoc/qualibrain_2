3ac58a491aeef27c2d6aab07fa44824c50f22553
Renan Brito <renan.brito@qualidoc.com.br>
2021-04-09 10:10:14 -0300

    Teste da API da Totvs com Axios e com cUrl

---
+++ api_teste_js.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>QualiBrain - Qualidoc</title>
+    <link rel="stylesheet" href="https://unpkg.com/buefy/dist/buefy.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css">
+
+    <!-- Custom fonts for this template-->
+    <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="./vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="./css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="./css/comum.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+</body>
+</html>
+
+<script src="https://unpkg.com/vue"></script>
+<script src="https://unpkg.com/buefy/dist/buefy.min.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script>
+    new Vue({
+        el: '#app',
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+          loadAsyncData() {
+            axios.get('http://ultraclinica.totvscloud.com.br:2001/RMS/RMSSERVICES/ReportWebAPI/api/v1/SaleHistory/GetCompareSales/RMS/RMSSERVICES/ReportWebAPI/api/v1/SaleHistory/GetByDate?filial=1007&dataVenda=2021-04-08').then((response) => {
+
+            }).catch((error) => {
+                throw error
+            })
+          }
+        },
+        mounted() {
+            this.loadAsyncData()
+        }
+    })
+</script>
